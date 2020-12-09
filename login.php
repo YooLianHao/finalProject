282f8fb4d4f4868220cdf343a099a13906394fac
YooLianHao <lianhao171@gmail.com>
2020-12-09 10:52:52 +0800

    a

---
+++ login.php
@@ -0,0 +1,150 @@
+<?php
+$servername = "localhost";  //localost for local PC or use IP
+$username = "root";
+$password = "";
+$database = "e-voting";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database);
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['userName']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['userPassword']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+      }
+      //modify this area -Jay
+      $sql = "Select * from user where users_name = '" . $_POST["user"] . "'"; // still not sure since i dont have the database
+        if(!isset($_COOKIE["member_login"])) {
+            $sql .= " AND member_password = '" . md5($_POST[""]) . "'";
+	}
+        $result = mysqli_query($conn,$sql);
+	$user = mysqli_fetch_array($result);
+	if($user) {
+			$_SESSION["member_id"] = $user["member_id"];
+			
+			if(!empty($_POST["remember"])) {
+				setcookie ("member_login",$_POST["member_name"],time()+ (10 * 365 * 24 * 60 * 60));
+			} else {
+				if(isset($_COOKIE["member_login"])) {
+					setcookie ("member_login","");
+				}
+			}
+	} else {
+		$message = "Invalid Login";
+	}
+   }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title> E-voting System | Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="../../index2.html"><b>E-Voting</b>System</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+      <!--/.login form -->
+      <form action="../../index3.html" method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" name="userName" placeholder="Username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" name="userPassword"placeholder="Password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember" name="remember" <?php if(isset($_COOKIE["users"])) { ?> checked <?php } ?> />
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center mb-3">
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
+        </a>
+      </div>
+      <!-- /.social-auth-links -->
+
+      <p class="mb-1">
+        <a href="forgot-password.html">I forgot my password</a>
+      </p>
+      <p class="mb-0">
+        <a href="userRegistration.php" class="text-center">Register New Account</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src=/dist/js/adminlte.min.js"></script>
+
+</body>
+</html>
---
+++ userRegistration.php
@@ -0,0 +1,166 @@
+<?php
+$servername = "localhost";  //localost for local PC or use IP
+$username = "root";
+$password = "";
+$database = "e-voting";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+//if user click create button
+if(isset($_POST['insert'])){  
+	//recieved input value
+	$userId=$_POST['userId']; 
+	$userName=$_POST['userName'];
+
+	$userPassword=$_POST['userPassword'];
+	$userPhoneNumber=$_POST['userPhoneNumber'];
+	$userEmail=$_POST['userEmail'];
+
+	$sql="insert into user values('$userId','$userName','$userPhoneNumber','$userEmail','$userPassword')";
+	//echo $sql="insert into user values('$userId','$userName','$userPhoneNumber','$userEmail','$userPassword')";
+
+	$result = $conn->query($sql);
+	
+}
+
+if(isset($_GET['id'])){
+$id=$_GET['id'];  //received driver id from viewDrivers.php
+$sql="select *from user where id ='$id'";
+$result = $conn->query($sql);
+
+if($result->num_rows > 0) {	
+	while($row = $result->fetch_assoc()){
+
+	$id=$row['id'];
+	$name=$row['name'];
+	$contactNo=$row['contactNo'];
+	$email=$row['email'];
+
+		}		
+	}
+}
+
+
+if(isset($_POST['update'])){
+	$userId=$_POST['userId'];
+	$userName=$_POST['userName'];
+	$userPhoneNumber=$_POST['userPhoneNumber'];
+	$userEmail=$_POST['userEmail'];
+
+	echo $sql="update user set name= '$userName', contactNo= '$userPhoneNumber', email = '$userEmail' 
+	where id = '$userId'";
+	$result=$conn->query($sql);
+	echo "<script>window.location.assign('viewUsers.php');</script>";
+
+}
+?>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.8/css/all.css">
+
+
+<div class="container">
+
+
+<div class="card bg-light" style="max-width: 400px; margin:0 auto;" >
+<article class="card-body mx-auto" style="max-width: 400px;">
+	<h4 class="card-title mt-3 text-center" >
+	<?php
+		if(isset($_GET['id'])){
+			echo "Update User";
+		}else{
+			//echo "<body style=background-color:powderblue;>";
+			echo "User Registration";
+		}	
+	?>
+	</h4>
+
+	<p class="text-center">Get started with your free account</p>
+	
+	
+	<form name="userRegistration" action="userRegistration.php" method="POST">
+	<div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+		<input name="userId" class="form-control" placeholder="User ID" type="text" value="<?php 
+		if(isset($_GET['id'])) { echo $id; } ?>">
+    </div> <!-- form-group// -->
+	
+    <div class="form-group input-group">
+		<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+		 </div>
+        <input name="userName" class="form-control" placeholder="Full name" type="text"  value="<?php 
+		if(isset($_GET['id'])) { echo $name; } ?>">
+    </div> <!-- form-group// -->
+
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-envelope"></i> </span>
+		 </div>
+        <input name="userEmail" class="form-control" placeholder="Email address" type="email"  value="<?php 
+		if(isset($_GET['id'])) { echo $email; } ?>">
+    </div> <!-- form-group// -->
+
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-phone"></i> </span>
+		</div>	
+    	<input name="userPhoneNumber" class="form-control" placeholder="Phone number" type="text"  value="<?php 
+		if(isset($_GET['id'])) { echo $contactNo; } ?>">
+    </div> <!-- form-group// -->
+
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input name="userPassword" class="form-control" placeholder="Create password" type="password">
+    </div> <!-- form-group// -->
+
+    <div class="form-group input-group">
+    	<div class="input-group-prepend">
+		    <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+		</div>
+        <input name="userRepeatPassword" class="form-control" placeholder="Repeat password" type="password">
+    </div> <!-- form-group// --> 
+
+    <div class="form-group">
+        
+		<?php
+		if(isset($_GET['id'])){
+			echo '<button type ="submit" class ="btn btn-primary 
+			btn-block" name="update"> Update User </buton>';
+		}else{
+			echo '<button type ="submit" class ="btn btn-primary
+			 btn-block" name ="insert"> Create Account </buton>';
+		}	
+	?>  
+
+    </div> <!-- form-group// -->
+
+    <p class="text-center">Have an account? <a href="">Log In</a> </p>                                                                 
+</form>
+</article>
+</div> <!-- card.// -->
+
+</div> 
+<!--container end.//-->
+
+<br><br>
+
+<article class="bg-secondary mb-3">  
+
+
+</article>
+
