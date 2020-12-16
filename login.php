742689eed1aba38d4c56c9c02443bb59c5be84a8
EnzoJay030619 <61226073+EnzoJay030619@users.noreply.github.com>
2020-12-16 14:43:17 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,57 +1,4 @@
-<?php
-$servername = "localhost";  //localost for local PC or use IP
-$username = "root";
-$password = "";
-$database = "e-voting";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password,$database);
-   
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $myusername = mysqli_real_escape_string($db,$_POST['userName']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['userPassword']); 
-      
-      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
-      
-      $count = mysqli_num_rows($result);
-      
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      if($count == 1) {
-         session_register("myusername");
-         $_SESSION['login_user'] = $myusername;
-         
-         header("");
-      }else {
-         $error = "Your Login Name or Password is invalid";
-      }
-      //modify this area -Jay
-      $sql = "Select * from user where users_name = '" . $_POST["user"] . "'"; // still not sure since i dont have the database
-        if(!isset($_COOKIE["member_login"])) {
-            $sql .= " AND member_password = '" . md5($_POST[""]) . "'";
-	}
-        $result = mysqli_query($conn,$sql);
-	$user = mysqli_fetch_array($result);
-	if($user) {
-			$_SESSION["member_id"] = $user["member_id"];
-			
-			if(!empty($_POST["remember"])) {
-				setcookie ("member_login",$_POST["member_name"],time()+ (10 * 365 * 24 * 60 * 60));
-			} else {
-				if(isset($_COOKIE["member_login"])) {
-					setcookie ("member_login","");
-				}
-			}
-	} else {
-		$message = "Invalid Login";
-	}
-   }
-?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -75,16 +22,19 @@ $conn = new mysqli($servername, $username, $password,$database);
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="../../index2.html"><b>E-Voting</b>System</a>
+    <a href=""><b>E-Voting</b>System</a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
       <!--/.login form -->
-      <form action="../../index3.html" method="post">
+      <form action="controller/loginProcess.php" method="post">
+      <?php if (isset($_GET['error'])) { ?>
+     		<p class="error"><?php echo $_GET['error']; ?></p>
+     	<?php } ?>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" name="userName" placeholder="Username">
+          <input type="email" class="form-control" name="email" placeholder="Email">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-envelope"></span>
@@ -92,7 +42,7 @@ $conn = new mysqli($servername, $username, $password,$database);
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" name="userPassword"placeholder="Password">
+          <input type="password" class="form-control" name="pass"placeholder="Password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -110,21 +60,11 @@ $conn = new mysqli($servername, $username, $password,$database);
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block" name = "submit" id="">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
       </form>
-
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
       <!-- /.social-auth-links -->
 
       <p class="mb-1">
