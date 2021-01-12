f3f4d6a928d235da2e62c818857b77c5c30ca817
EnzoJay030619 <61226073+EnzoJay030619@users.noreply.github.com>
2021-01-13 04:40:48 +0800

    Delete votesystem directory

--- votesystem/admin/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>EVoting</b>
-</div>
-    <strong>Copyright &copy; 2020-2021 <a href="">Southern University College</a>.</strong>
-    All rights reserved.
-  </footer>
