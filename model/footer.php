6fdcd824e4381ae8afd54ac2df6b944dffe4fa9a
EnzoJay030619 <61226073+EnzoJay030619@users.noreply.github.com>
2021-01-13 04:34:33 +0800

    Delete model directory

--- model/footer.php
+++
@@ -1,7 +0,0 @@
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2020-2021 <a href="">Southern University College</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Credits</b>Admin Lte
-    </div>
-  </footer>
